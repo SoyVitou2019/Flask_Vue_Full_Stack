cbe46cfc32a4bb5fc5859bb70563bbdc4b3ef3d4
Soy Vitou <soyvitou999@gmail.com>
2023-03-09 12:33:36 +0700

    version 5

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <Home />
+  <div>
+    <Home />
+    <Contact_Us />
+    <login />
+  </div>
 </template>
 
 <script>
 import Home from "./components/Home.vue"
+import Contact_Us from "./components/Contact_us.vue"
+import login from "./components/login.vue"
 export default {
   components:{
-      Home
+      Home,
+      Contact_Us,
+      login
     },
   setup() {
    
     return {
-      Home
+      Home,
+      Contact_Us,
+      login
     }
   },
 }
